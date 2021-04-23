ecf7113f82ebb7e8b4d96a4a4c1fbd15a8128212
314ronin <pownmymail@gmail.com>
2021-04-23 14:03:07 +0300

    feat: Implement Burger menu

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('finex-task');
+//Burger Button
+const burgerBtn = document.querySelector('.jsBurgerBtn');
+const headerMenu = document.querySelector('.jsHederMenu');
+const body = document.querySelector('.body');
+
+burgerBtn.addEventListener('click', ()=> {
+	if (screen.width <= 1024) {
+
+		if(headerMenu.classList.contains('mobile-menu--active')) {
+			body.style.overflow='auto';
+			headerMenu.classList.remove('mobile-menu--active');
+			burgerBtn.classList.remove('mobile-menu--active');
+		} else {
+			headerMenu.classList.add('mobile-menu--active');
+			burgerBtn.classList.add('mobile-menu--active');
+			body.style.overflow='hidden';
+		}
+	}
+
+});
